888368756a828ecf93721fcaed6003b48a3f700b
Justin Coyne <justin@curationexperts.com>
2013-04-10 16:43:01 -0500

    Finished adding users

--- lib/hydra/role_management/roles_behavior.rb
+++ lib/hydra/role_management/roles_behavior.rb
@@ -12,6 +12,17 @@ module Hydra
 
       def show
       end
+
+      def add_user
+        u = ::User.find_by_user_key(params[:user_key])
+        if u
+          u.roles << @role
+          u.save!
+          redirect_to role_management.role_path(@role)
+        else
+          redirect_to role_management.role_path(@role), :flash=> {:error=>"Unable to find the user #{params[:user_key]}"}
+        end
+      end
     end
   end
 end
