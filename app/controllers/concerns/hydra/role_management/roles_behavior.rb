701d9a501bc5dfe0f9ac6d348a839e6ca5139023
Chris Colvard <cjcolvar@indiana.edu>
2013-05-20 23:54:10 -0400

    Update name of role and delete role

--- app/controllers/concerns/hydra/role_management/roles_behavior.rb
+++ app/controllers/concerns/hydra/role_management/roles_behavior.rb
@@ -11,17 +11,38 @@ module Hydra
       end
 
       def show
+        redirect_to role_management.edit_role_path(@role) if can? :edit, Role
       end
 
       def new
       end
 
+      def edit
+      end
+
       def create
         @role.name = params[:role][:name]
         if (@role.save)
-          redirect_to role_management.roles_path, notice: 'Role was successfully created.'
+          redirect_to role_management.edit_role_path(@role), notice: 'Role was successfully created.'
         else
           render action: "new"
+        end
+      end
+
+      def update
+        @role.name = params[:role][:name]
+        if (@role.save)
+          redirect_to role_management.edit_role_path(@role), notice: 'Role was successfully updated.'
+        else
+          render action: "edit"
+        end
+      end
+
+      def destroy
+        if (@role.destroy)
+          redirect_to role_management.roles_path, notice: 'Role was successfully deleted.'
+        else
+          redirect_to role_management.roles_path
         end
       end
 
