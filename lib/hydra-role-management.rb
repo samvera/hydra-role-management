bb56e2d5c9b2a7c5a4695951271c5c117f9716ab
Michael Klein <mbklein@gmail.com>
2013-10-03 14:44:34 -0400

    Support drawing routes on a path other than /roles

--- lib/hydra-role-management.rb
+++ lib/hydra-role-management.rb
@@ -1,2 +1,16 @@
 require "hydra/role_management"
 require 'bootstrap_forms'
+
+module Hydra
+  module RoleManagement
+    mattr_accessor :route_options
+    self.route_options = {}
+    
+    def self.draw_routes(router, opts={})
+      self.route_options = opts
+      router.instance_exec do
+        mount Hydra::RoleManagement::Engine => '/'
+      end
+    end
+  end
+end
