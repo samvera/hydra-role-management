5e28251f9fb1a7997da9980ebd4327c9bb2dcc3c
Justin Coyne <justin@curationexperts.com>
2013-12-20 13:48:12 -0600

    Don't check guest? unless DeviseGuests is installed. Fixes #10

--- spec/lib/user_roles_spec.rb
+++ spec/lib/user_roles_spec.rb
@@ -1,26 +1,54 @@
 require 'spec_helper'
 
 describe Hydra::RoleManagement::UserRoles do
-  subject do
-    User.create!(email: 'fred@example.com', password: 'password')
+  let(:librarian) { Role.create!(name: 'librarian') }
+  describe "a real user" do
+    subject do
+      User.create!(email: 'fred@example.com', password: 'password')
+    end
+
+    it "should have admin?" do
+      subject.should_not be_admin
+    end
+
+    describe "with roles" do
+      before do
+        subject.roles << librarian
+      end
+
+      its(:roles) { should eq [librarian] }
+      its(:groups) { should include('registered', 'librarian') }
+    end
   end
 
-  it "should have admin?" do
-    subject.should_not be_admin
-  end
+  describe "when DeviseGuests is installed" do
+    before do
+      stub_const("DeviseGuests", true)
+    end
 
-  it "should have roles" do
-    subject.roles.should == []
-    subject.roles << Role.create!(name: 'librarian')
-    subject.roles.first.name.should == 'librarian'
+    describe "a real user" do
+      subject do
+        User.create!(email: 'fred@example.com', password: 'password')
+      end
 
-  end
+      it { should_not be_admin }
 
-  it "should have groups" do
-    subject.roles.should == []
-    subject.roles << Role.create!(name: 'librarian')
-    subject.save!
-    subject.groups.should include('registered', 'librarian')
+      describe "with roles" do
+        before do
+          subject.roles << librarian
+        end
+
+        its(:groups) { should include('registered', 'librarian') }
+      end
+    end
+    describe "a guest user" do
+      subject do
+        User.create!(email: 'fred@example.com', password: 'password', guest: true)
+      end
+
+      it { should_not be_admin }
+      its(:groups) { should be_empty }
+    end
   end
 
 end
