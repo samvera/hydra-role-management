81dbe56d7f1c486d4e4bbbeed6aa826329bbc7c5
Mike Korcynski <mkorcy@gmail.com>
2014-12-16 15:17:12 -0500

    adds use of cancel behavior

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -1,5 +1,7 @@
 class UserRolesController < ApplicationController
   include Hydra::RoleManagement::UserRolesBehavior
+  include Hydra::RoleManagement::CancelBehavior
+  before_filter :check_for_cancel, :only => [:create, :update]
 end
 
 
