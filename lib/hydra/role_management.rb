36f5b6eb353317603cfec745afb932c9d66c9e16
Chris Colvard <chris.colvard@gmail.com>
2024-10-02 15:22:19 -0400

    Workaround zeitwerk and engine_cart issue

--- lib/hydra/role_management.rb
+++ lib/hydra/role_management.rb
@@ -10,6 +10,18 @@ module Hydra
       # Rails 4 should do this automatically:
       config.paths.add 'app/controllers/concerns', eager_load: true
       config.paths.add 'app/models/concerns', eager_load: true
+
+      config.before_configuration do
+        # rubocop:disable Style/IfUnlessModifier
+        # see https://github.com/fxn/zeitwerk#for_gem
+        # Blacklight puts a generator into LOCAL APP lib/generators, so tell
+        # zeitwerk to ignore the whole directory? If we're using zeitwerk
+        #
+        # See: https://github.com/cbeer/engine_cart/issues/117
+        if ::Rails.try(:autoloaders).try(:main).respond_to?(:ignore)
+          ::Rails.autoloaders.main.ignore(::Rails.root.join('lib', 'generators'))
+        end
+      end
     end
   end
 end
